b4ed454fa18d31dde30db667d1533d2d9779d75d
KimThanh1801 <hokimthanh1234@gmail.com>
2025-04-29 07:49:37 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Form from './Component/Form'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+  return (
+    <>
+      <Form/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/data.js
@@ -0,0 +1,44 @@
+export function getData() {
+  return [
+    {
+      id: "1",
+      name: "2 giuong doi lon",
+      myfile: "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/04/hinh-nen-doremon-cho-dien-thoai-dang-yeu-nhat.jpg.webp",
+      type_room: "Phong Family",
+      number: "4 nguoi lon",
+      area: "45m",
+      price: 3242,
+      oldprice: 23432
+    },
+    {
+      id: "2",
+      name: "2 giuong doi lon",
+      myfile: "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/04/hinh-nen-doremon-cho-dien-thoai-dang-yeu-nhat.jpg.webp",
+      type_room: "Phong Family",
+      number: "4 nguoi lon",
+      area: "45m",
+      price: 3242,
+      oldprice: 23432
+    },
+    {
+      id: "3",
+      name: "2 giuong doi lon",
+      myfile: "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/04/hinh-nen-doremon-cho-dien-thoai-dang-yeu-nhat.jpg.webp",
+      type_room: "Phong Family",
+      number: "4 nguoi lon",
+      area: "45m",
+      price: 3242,
+      oldprice: 23432
+    },
+    {
+      id: "4",
+      name: "2 giuong doi lon",
+      myfile: "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/04/hinh-nen-doremon-cho-dien-thoai-dang-yeu-nhat.jpg.webp",
+      type_room: "Phong Family",
+      number: "4 nguoi lon",
+      area: "45m",
+      price: 3242,
+      oldprice: 23432
+    }
+  ];
+}
